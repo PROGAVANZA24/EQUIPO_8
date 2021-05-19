bacc501cf96ed709e5f9a27ec97d8c7daeae3d0a
Clara Itzzel Montes Sánchez <claraimontes15@gmail.com>
2021-05-18 19:04:20 -0500

    Se importan las clases MenuAdministrador.py

---
+++ MenuAdministrador.py
@@ -0,0 +1,15 @@
+from ProgramaPrincipal import Tema
+from ProgramaPrincipal import Video
+from ProgramaPrincipal import CursoTemaVideo
+from ProgramaPrincipal import CursoTema
+from ProgramaPrincipal import Curso
+from ProgramaPrincipal import Empleado
+
+print('Qué tabla desea consultar')
+print('T= Tabla de Tema')
+print('V= Tabla de Video')
+print('CTV= Tabla de Curso Tema Video')
+print('CT= Tabla de Curso Tema')
+print('C=Tabla de Curso')
+print('E= Tabla de Empleados')
+Tabla=input()
