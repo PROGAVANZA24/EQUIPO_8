ccbc601e9ff10cb758d3511c87942ed23d44cd9c
dohot37 <luis_donando20@outlook.com>
2021-05-18 19:12:19 -0500

    se agregaron las consultas de la tabla tema MenuAdministrador.py

--- MenuAdministrador.py
+++ MenuAdministrador.py
@@ -13,3 +13,29 @@ print('CT= Tabla de Curso Tema')
 print('C=Tabla de Curso')
 print('E= Tabla de Empleados')
 Tabla=input()
+
+
+from ProgramaPrincipal import Tema
+if (Tabla=='T'):
+    Tema1={
+            "IdTema":1,
+            "Nombre":"Combinaciones y Permutaciones"
+        }
+    Tema2={
+            "IdTema":2,
+            "Nombre":"Mezclas homogeneas y heterogeneas"
+        }
+    print('Cómo desea hacer su consulta')
+    print('a=Por Id')
+    print('b= Desea ver la información completa')
+    respuesta=input()
+    if (respuesta=='a'):
+        a=Tema1['IdTema']
+        print('IdTema1: ',a)
+        a=Tema2['IdTema']
+        print('IdTema2: ',a)
+    else:
+        print(Tema1)
+        print(Tema2)
+f=open('c:/Users/clara/OneDrive/Escritorio/Programas/Tema.txt','w',encoding='utf8')
+f.close
